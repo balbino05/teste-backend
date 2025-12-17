f9d1e68ae049cb3a16ef86bdf953c3fd85927b9e
Ivan Cassio Balbino Dias Amaral <ivan.amaral@a4pmanalytics.onmicrosoft.com>
2025-12-17 17:04:12 -0300

    fix: simplifica Handler e RouteServiceProvider

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -27,40 +26,6 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $e
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
-     */
-    public function render($request, Throwable $e)
-    {
-        if ($request->expectsJson() || $request->is('api/*') || $request->is('*')) {
-            if ($e instanceof ValidationException) {
-                return response()->json([
-                    'message' => 'The given data was invalid.',
-                    'errors' => $e->errors(),
-                ], 422);
-            }
-
-            if ($e instanceof \DomainException) {
-                return response()->json([
-                    'error' => $e->getMessage(),
-                ], 400);
-            }
-
-            // Retornar erro genérico para outras exceções
-            return response()->json([
-                'error' => $e->getMessage(),
-            ], 500);
-        }
-
-        return parent::render($request, $e);
     }
 }
 
