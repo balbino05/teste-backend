2b73812173991809e367914c87d36e3344810b1f
Ivan Cassio Balbino Dias Amaral <ivan.amaral@a4pmanalytics.onmicrosoft.com>
2025-12-17 16:50:54 -0300

    fix: adiciona configurações de teste e melhora tratamento de exceções

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,6 +27,45 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($request->expectsJson() || $request->is('api/*') || $request->is('*')) {
+            if ($e instanceof ValidationException) {
+                return response()->json([
+                    'message' => 'The given data was invalid.',
+                    'errors' => $e->errors(),
+                ], 422);
+            }
+
+            if ($e instanceof \DomainException) {
+                return response()->json([
+                    'error' => $e->getMessage(),
+                ], 400);
+            }
+
+            // Em ambiente de teste, retornar mais informações sobre o erro
+            if (app()->environment('testing')) {
+                return response()->json([
+                    'error' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                    'trace' => $e->getTraceAsString(),
+                ], 500);
+            }
+        }
+
+        return parent::render($request, $e);
     }
 }
 
