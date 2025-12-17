d5c13ebc66d73be6b5efa48a3df9af1a04c62b97
Ivan Cassio Balbino Dias Amaral <ivan.amaral@a4pmanalytics.onmicrosoft.com>
2025-12-17 17:19:45 -0300

    fix: adicionado rate limit

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\TransferController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/transfer', [TransferController::class, 'transfer']);
+Route::post('/transfer', [TransferController::class, 'transfer'])
+    ->middleware('throttle:60,1'); // Rate limiting: 60 requisições por minuto
 
